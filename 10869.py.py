36f462533ac76a096e5e7b3e04bc5abf4e163822
jaehee72 <55860058+jaehee72@users.noreply.github.com>
2019-10-03 15:27:31 +0900

    <10869>.py

---
+++ 10869.py.py
@@ -0,0 +1,20 @@
+a, b= map(int, input(). split())
+def plus(a,b):
+    return a+b
+print(plus(a,b))
+
+def minus(a,b):
+    return a-b
+print(minus(a,b))
+
+def multiplication(a,b):
+    return(a*b)
+print(multiplication(a,b))
+
+def share(a,b):
+    return(a//b)
+print(share(a,b))
+
+def rest(a,b):
+    return(a%b)
+print(rest(a,b))
